8042da1cb2531da6dc4b19187abaca2f64622b69
Yonghan Lee <lyhan12@umd.edu>
2024-02-04 17:16:27 -0500

    discard normal reading in fetchPly

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -109,7 +109,7 @@ def fetchPly(path):
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
     colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
-    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    normals = np.zeros_like(positions) #np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
 def storePly(path, xyz, rgb):
